051f7168bd5085c22a5b0681de863add9c41a91a
Jovan Isailovic <knife747@gmail.com>
2024-05-23 17:03:53 +0200

    Added subject, department and classroom create forms

---
+++ client/src/app/pages/department/create/create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-department-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './create.component.html'
+})
+
+export class DepartmentCreate {
+
+}
---
+++ client/src/app/pages/subject/create/create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './create.component.html'
+})
+
+export class SubjectCreate {
+
+}
